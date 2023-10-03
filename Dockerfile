c8099506bf2dba27a479d64faae4f98ad3fdcaf2
Varun Saini <61795485+VarunSaini02@users.noreply.github.com>
2023-10-03 13:57:44 -0500

    Add boilerplate for Go engine and Docker set up (#16)
    * Add boilerplate for Go engine and Docker set up
    
    * Move Dockerfiles to src folders

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.8-alpine as engine
-COPY src/engine/* .
-RUN pip install flask
-CMD ["python", "engine.py"]
-
-FROM python:3.8-alpine as play-game
-COPY src/play-game/* .
-RUN pip install flask
-CMD ["python", "play-game.py"]
---
+++ src/engine/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func home(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, map[string]string{"message": "Engine is running"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", home)
+	router.Run("0.0.0.0:5000")
+}
