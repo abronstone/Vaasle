f493b1883e981369a6cbfff0b2c6ff1799151376
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-10-03 16:48:26 -0500

    Add CORS support to main.go in engine

--- src/engine/main.go
+++ src/engine/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,14 @@ func home(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+
+	// Add CORS middleware
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true // Allow any origin (not recommended for production)
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+	router.Use(cors.New(config))
+
 	router.GET("/", home)
 	router.Run("0.0.0.0:5000")
 }
