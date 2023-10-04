c9dc61922c25c5ecb3553e1fcf24200931b03a97
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-10-04 10:42:12 -0500

    Improved start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker-compose up -d --build --remove-orphans
 docker-compose exec -it cli /bin/sh -c "go run cli.go"
 echo "Closing down containers"
-docker stop $(docker ps -a -q)
+docker-compose down
