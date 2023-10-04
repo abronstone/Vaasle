04f96221dea7b45091b1fedda52ad70da68af30a
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-10-04 13:24:26 -0500

    Remove CORS for engine main.go

--- src/engine/main.go
+++ src/engine/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,9 +12,6 @@ func home(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
-
-	// Add CORS middleware
-	router.Use(cors.Default())
 
 	router.GET("/", home)
 	router.Run("0.0.0.0:5000")
