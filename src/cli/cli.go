445b915ca84a9c88bcc3d804b8d74d2ec861c864
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-10-05 14:09:03 -0500

    Create simple GET request to play-game

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -3,11 +3,26 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Sending GET request to play-game...")
+	res, err := http.Get("http://play-game:5001/")
+	if err != nil {
+		fmt.Println("The GET request to play-game threw an error:", err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("The GET request to play-game threw an error:", err)
+	} else {
+		fmt.Println("The GET request to play-game returned:", string(body))
+	}
 
 	fmt.Print("Enter text (entering will cause containers to shut down): ")
 	text, _ := reader.ReadString('\n')
