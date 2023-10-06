9181c64d80dfd89152ff6001691c95e3814c78b4
Varun Saini <61795485+VarunSaini02@users.noreply.github.com>
2023-10-05 20:17:57 -0500

    Set up back-and-forth pings between engine and play-game

--- src/play-game/playGame.go
+++ src/play-game/playGame.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+func pingEngine(c *gin.Context) {
+	res, err := http.Get("http://engine:5001/")
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": "Ping to engine failed"})
+		return
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": "Ping to engine failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, string(bodyBytes))
+}
 
 func home(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, map[string]string{"message": "Play game is running"})
@@ -18,5 +36,7 @@ func main() {
 	router.Use(cors.Default())
 
 	router.GET("/", home)
+	router.GET("/pingEngine", pingEngine)
+
 	router.Run("0.0.0.0:5001")
 }
