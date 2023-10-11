dcfdd124f1bd1e4f197cfc88bff279d858550bd0
Varun Saini <61795485+VarunSaini02@users.noreply.github.com>
2023-10-11 11:28:51 -0500

    Add GameExposed type

--- src/structs/game_structs.go
+++ src/structs/game_structs.go
@@ -14,3 +14,22 @@ type Game struct {
 	State    string       `json:"state"`
 	Word     string       `json:"-"`
 }
+
+// A Wordle game with an exportable public Word.
+// Only for Mongo and Engine communication.
+type GameExposed struct {
+	Metadata GameMetadata `json:"metadata"`
+	Guesses  [][2]string  `json:"guesses"`
+	State    string       `json:"state"`
+	Word     string       `json:"word"`
+}
+
+// Convert a GameExposed to a Game (remove the ability to export Word to JSON).
+func (g *GameExposed) ConvertToGame() *Game {
+	return &Game{
+		Metadata: g.Metadata,
+		Guesses:  g.Guesses,
+		State:    g.State,
+		Word:     g.Word,
+	}
+}
