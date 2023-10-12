130247e983386e6b88bbbfa6fb2380d7ca8ead47
Varun Saini <61795485+VarunSaini02@users.noreply.github.com>
2023-10-12 13:17:26 -0500

    Remove '--remove-orphans'

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker-compose up -d --build --remove-orphans
+docker-compose up -d --build
 docker-compose exec -it cli /bin/sh -c "go run cli.go"
 echo "Closing down containers"
 docker-compose down
