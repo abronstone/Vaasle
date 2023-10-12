d569ffcb979eade8639c7ad9157437ea52d19a20
Aaron Bronstone <bronstonea@carleton.edu>
2023-10-11 20:55:26 -0500

    Added BSON encoding to game structs

--- src/structs/game_structs.go
+++ src/structs/game_structs.go
@@ -2,26 +2,26 @@ package structs
 
 // The metadata of a Wordle game.
 type GameMetadata struct {
-	GameID     string `json:"gameID"`
-	WordLength int    `json:"wordLength"`
-	MaxGuesses int    `json:"maxGuesses"`
+	GameID     string `json:"gameID" bson:"gameid"`
+	WordLength int    `json:"wordLength" bson:"wordlength"`
+	MaxGuesses int    `json:"maxGuesses" bson:"maxguesses"`
 }
 
 // A Wordle game.
 type Game struct {
-	Metadata GameMetadata `json:"metadata"`
-	Guesses  [][2]string  `json:"guesses"`
-	State    string       `json:"state"`
-	Word     string       `json:"-"`
+	Metadata GameMetadata `json:"metadata" bson:"metadata"`
+	Guesses  [][2]string  `json:"guesses" bson:"guesses"`
+	State    string       `json:"state" bson:"state"`
+	Word     string       `json:"-" bson:"word"`
 }
 
 // A Wordle game with an exportable public Word.
 // Only for Mongo and Engine communication.
 type GameExposed struct {
-	Metadata GameMetadata `json:"metadata"`
-	Guesses  [][2]string  `json:"guesses"`
-	State    string       `json:"state"`
-	Word     string       `json:"word"`
+	Metadata GameMetadata `json:"metadata" bson:"metadata"`
+	Guesses  [][2]string  `json:"guesses" bson:"guesses"`
+	State    string       `json:"state" bson:"state"`
+	Word     string       `json:"word" bson:"word"`
 }
 
 // Convert a GameExposed to a Game (remove the ability to export Word to JSON).
