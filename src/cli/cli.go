5df9f94155689a81290c6cdcb25e39a7e69726d7
spatino1234 <107152515+spatino1234@users.noreply.github.com>
2023-10-13 20:49:30 -0700

    43 Playgame CLI (#60)
    * making updates to cli.go file and docker
    
    * Refactor cli to use hardcoded structs
    Co-authored-by: spatino1234 <spatino1234@users.noreply.github.com>
    
    * Cleanup output and formatting
    
    ---------
    
    Co-authored-by: Serafin Patino <serafinpatino@Serafins-MacBook-Pro.local>
    Co-authored-by: AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -2,32 +2,188 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+type Guess struct {
+	Id    string `json:"id"`
+	Guess string `json:"guess"`
+}
 
+type Game struct {
+	Metadata GameMetadata `json:"metadata" bson:"metadata"`
+	Guesses  [][2]string  `json:"guesses" bson:"guesses"`
+	State    string       `json:"state" bson:"state"`
+}
+
+type GameMetadata struct {
+	GameID     string `json:"gameID" bson:"gameid"`
+	WordLength int    `json:"wordLength" bson:"wordlength"`
+	MaxGuesses int    `json:"maxGuesses" bson:"maxguesses"`
+}
+
+func main() {
+	err := ping_play_game()
+
+	if err != nil {
+		return
+	}
+
+	currentGame, err := initialize_new_game()
+
+	if currentGame == nil {
+		return
+	}
+
+	fmt.Println("New game created with ID:", currentGame.Metadata.GameID)
+
+	gameID := currentGame.Metadata.GameID
+
+	// Make guesses until the game is won or lost (make_guess returns nil)
+	for {
+		fmt.Println("Guess a word:")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		guess := scanner.Text()
+		var cleanedUpGuess = strings.ReplaceAll(guess, " ", "")
+
+		if len(cleanedUpGuess) != currentGame.Metadata.WordLength {
+			fmt.Println("Your word doesn't match the required length. Try again.")
+			continue
+		}
+
+		// Make a guess and get the corrections
+		lastGuess, err := make_guess(gameID, guess)
+
+		if err != nil || lastGuess == "" {
+			return
+		}
+
+		output := ""
+		// Print out the corrections
+		for _, correction := range lastGuess {
+			switch string(correction) {
+			case "G":
+				output += "🟩"
+			case "Y":
+				output += "🟧"
+			default:
+				output += "⬛"
+			}
+		}
+
+		fmt.Println(output)
+	}
+
+}
+
+func ping_play_game() error {
 	fmt.Println("Sending GET request to play-game...")
 	res, err := http.Get("http://play-game:5001/")
 	if err != nil {
 		fmt.Println("The GET request to play-game threw an error:", err)
+		return err
 	}
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println("The GET request to play-game threw an error:", err)
+		return err
 	} else {
 		fmt.Println("The GET request to play-game returned:", string(body))
 	}
 
-	fmt.Println("Please also check out our frontend at http://localhost:3000/")
+	return nil
+}
 
-	fmt.Print("Enter text (entering will cause containers to shut down): ")
-	text, _ := reader.ReadString('\n')
+func initialize_new_game() (*Game, error) {
+	res, err := http.Post("http://play-game:5001/newGame", "application/json", nil)
 
-	fmt.Println("You entered:", text)
+	// If play-game is down, return an error
+	if err != nil {
+		fmt.Println("Failed to create a new game")
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	// Create a newGame variable and unmarshal the response body into it
+	newGame := Game{}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("error: Failed read response body from play-game")
+		return nil, err
+	}
+
+	// format the json response data from bodybytes, have it conform to Game type. Make it filled.
+	err = json.Unmarshal(bodyBytes, &newGame)
+	if err != nil {
+		fmt.Println("error: Failed to unmarshal response body from engine")
+		return nil, err
+	}
+
+	return &newGame, nil
+}
+
+func make_guess(gameID string, guess string) (string, error) {
+	// Make a guess of type Guess
+	guessStruct := Guess{
+		Id:    gameID,
+		Guess: guess,
+	}
+	// Convert guessStruct to JSON
+	jsonData, err := json.Marshal(guessStruct)
+	if err != nil {
+		fmt.Println("Failed to marshal guess:", err)
+		return "", err
+	}
+
+	// Use strings.NewReader to convert the JSON string to an io.Reader
+	res, err := http.Post("http://play-game:5001/makeGuess", "application/json", strings.NewReader(string(jsonData)))
+
+	if err != nil {
+		fmt.Println("Failed to create a new game")
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	// Create a newGame variable and unmarshal the response body into it
+	currentGame := Game{}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("error: Failed read response body from play-game")
+		return "", err
+	}
+
+	// Format the json response data from body bytes, have it conform to Game type. Make it filled.
+	err = json.Unmarshal(bodyBytes, &currentGame)
+	if err != nil {
+		fmt.Println("error: Failed to unmarshal response body from play-game")
+		return "", err
+	}
+
+	// Check if the game is won or lost
+	if currentGame.State == "won" {
+		fmt.Print("Congratulations! You won the game!")
+		return "", err
+	}
+	if currentGame.State == "lost" {
+		fmt.Print("Unlucky! You suck at the game delete wordle and your mother hates you!")
+		return "", err
+	}
+
+	// Get the corrections for hinting of the last guess made
+	var lastGuessCorrections string = currentGame.Guesses[len(currentGame.Guesses)-1][1]
+
+	return lastGuessCorrections, nil
 }
