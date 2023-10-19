cc6eb51a61f079d283e818cc3d4371a37ea00f9c
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-10-18 21:41:45 -0500

    Commented out exposing solution for now

--- src/frontend/src/components/Modal.js
+++ src/frontend/src/components/Modal.js
@@ -6,14 +6,14 @@
         {isCorrect && (
         <div>
             <h1>You Win!</h1>
-            <p className="solution">{solution}</p>
+            {/* <p className="solution">{solution}</p> */}
             <p>You found the solution in {turn} guesses :)</p>
         </div>
         )}
         {!isCorrect && (
         <div>
             <h1>Nevermind</h1>
-            <p className="solution">{solution}</p>
+            {/* <p className="solution">{solution}</p> */}
             <p>Better luck next time :)</p>
         </div>
         )}
