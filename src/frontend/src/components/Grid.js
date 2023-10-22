b37768f118f5a5a119af9e9da612752ba7e74bca
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-10-21 23:28:12 -0500

    Fix: Incorrect num of rows

--- src/frontend/src/components/Grid.js
+++ src/frontend/src/components/Grid.js
@@ -4,15 +4,19 @@ import Row from "./Row";
 
 export default function Grid({ guesses, currentGuess, turn }) {
   console.log("grid rendered");
+  console.log("guesses inside grid", guesses);
+
   return (
     <div>
-      {Object.keys(guesses).map((key, i) => {
-        if (turn === i) {
-          return <Row key={i} currentGuess={currentGuess} />;
-        }
-        console.log("guesses in Grid", guesses);
-        return <Row key={i} guesses={guesses} />;
-      })}
+     {/* Render as many rows as there are turns, where the last row is always filled with currentGuess */}
+     <p>Turn {turn}</p>
+      {[...Array(turn)].map((_, i) => {
+        console.log("i:", i);
+        return (
+        <Row key={i} guesses={guesses} />
+      )})}
+      <Row guesses={guesses[turn]} currentGuess={currentGuess} />
     </div>
   );
 }
+
