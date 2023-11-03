3ebd61777ff3c829fd5c5a47c7b2b4844b141053
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-11-03 17:22:14 -0500

    Place Auth0 information in .env.local

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,10 +4,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 
+const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const auth0ClientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-aj4r5fkdxle6ccqk.us.auth0.com"
-    clientId="G3zHxybsKblaoLOQ3xYvI6MtlqrjIPAh"
+    domain={auth0Domain}
+    clientId={auth0ClientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
