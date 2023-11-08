9870fd1f716d6597aa418de33ae834442ec347f9
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-11-07 23:19:29 -0600

    Implement MVP MultiplayerGame component

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect, useCallback } from "react";
 import CurrentUserGame from "./CurrentUserGame";
 import MultiplayerGame from "./MultiplayerGame";
-import { newGameApi } from "./components/util/apiCalls";
+import { newGameApi, getExternalUserGamesApi } from "./components/util/apiCalls";
 import { useAuth0 } from "@auth0/auth0-react";
+import ErrorBadge from "./components/ErrorBadge";
 
 function App() {
   const { isAuthenticated, user } = useAuth0();
   const [gameState, setGameState] = useState(null);
+  const [externalGamesState, setExternalGamesState] = useState(null)
+  const [error, setError] = useState(null)
   // TODO replace this with multiple pages 
   const [isMultiplayerEnabled, setIsMultiplayerEnabled] = useState(true);
 
   const initialGameState = useCallback(async () => {
     try {
-      if(!isAuthenticated) return;
+      if (!isAuthenticated) return;
       const maxGuesses = 5;
       const wordLength = 5;
       const initialData = await newGameApi(maxGuesses, wordLength, user.sub);
       setGameState(initialData);
     } catch (error) {
-      console.error("Failed to initialize game state:", error);
+      setError("Failed to initialize game state: " + error)
     }
   }, [isAuthenticated]);
 
@@ -27,11 +30,32 @@ function App() {
     initialGameState();
   }, [initialGameState]);
 
+  // Fetch state of externalUserGames for multiplayer when the current user submits a guess
+  // (the game state in the CurrentUserGame component changes)
+  useEffect(() => {
+    if (gameState != null && gameState.metadata.gameID != null && isMultiplayerEnabled) {
+      const fetchNewExternalGameState = async () => {
+        try {
+          const res = await getExternalUserGamesApi(gameState.metadata.gameID);
+          setExternalGamesState(res);
+        }
+        catch (e) {
+          setError("Failed to retrieve external user games" + e)
+        }
+      }
+
+      fetchNewExternalGameState();
+    }
+  }, [gameState, isMultiplayerEnabled])
+
   return (
     <div className="App">
-      <h1>Vassle</h1>
+      <h1>Vaasle</h1>
+      {error != null && <ErrorBadge text={error} />}
       <CurrentUserGame gameState={gameState} setGameState={setGameState} />
-      {isMultiplayerEnabled && isAuthenticated && <MultiplayerGame gameState={gameState} setGameState={setGameState} />}
+      {isMultiplayerEnabled && isAuthenticated && externalGamesState != null && Array.from(externalGamesState.externalUserGamesMap.values()).map((game) =>
+        <MultiplayerGame externalUserGameGuesses={game} />
+      )}
     </div>
   );
 }
--- src/frontend/src/MultiplayerGame.js
+++ src/frontend/src/MultiplayerGame.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // components
 import ExternalUserGrid from "./components/ExternalUserGrid";
 
-export default function MultiplayerGame() {
-  const corrections = [['green', 'green', 'gray', 'yellow', 'gray']]
+export default function MultiplayerGame({ externalUserGameGuesses }) {
+  const [corrections, setCorrections] = useState([])
 
+  useEffect(() => {
+    if (externalUserGameGuesses != null) {
+      const newCorrections = []
+      // Transform the guesses array into a format that the FE can understand
+      for (let turn of externalUserGameGuesses) {
+        const fullColorNameGuesses = []
+        for (let colorCode of turn) {
+
+          // Determine the color needed for FE based on the color code from the API
+          const color =
+            colorCode === "G"
+              ? "green"
+              : colorCode === "Y"
+                ? "yellow"
+                : "grey";
+          fullColorNameGuesses.push(color)
+        }
+        newCorrections.push(fullColorNameGuesses)
+
+      }
+      setCorrections(newCorrections)
+    }
+  }, [externalUserGameGuesses])
+  
   return (
     <div className="wordle-container">
       <ExternalUserGrid corrections={corrections} turn={corrections.length} />
