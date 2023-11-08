d7966e895d864ec20fc36b6589ccef173ca59d00
Serafin Patino <serafinpatino@Serafins-MacBook-Pro.local>
2023-11-07 21:48:58 -0600

    added stats to dev branch

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useCallback } from "react";
 import Wordle from "./Wordle";
 import { newGameApi } from "./components/util/apiCalls";
 import { useAuth0 } from "@auth0/auth0-react";
+import Stats from "./components/Stats";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   const { isAuthenticated, user } = useAuth0();
@@ -9,7 +12,7 @@ function App() {
 
   const initialGameState = useCallback(async () => {
     try {
-      if(!isAuthenticated) return;
+      if (!isAuthenticated) return;
       const maxGuesses = 5;
       const wordLength = 5;
       const initialData = await newGameApi(maxGuesses, wordLength, user.sub);
@@ -25,10 +28,26 @@ function App() {
   }, [initialGameState]);
 
   return (
-    <div className="App">
-      <h1>Vassle</h1>
-      <Wordle gameState={gameState} setGameState={setGameState} />
-    </div>
+    // * have the h1 tag always present, but switch between gameState and stats page
+    <Router>
+      <div className="App">
+        <h1>
+          Wordle (vaas.ai)
+          <Link to="/stats">Stats</Link>
+        </h1>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Wordle gameState={gameState} setGameState={setGameState} />
+            }
+          />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/frontend/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404 - Page not found</h2>
+    </div>
+  );
+}
