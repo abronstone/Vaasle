f4d1c74d42ec61b123f6771603c9fadec4908201
Serafin Patino <serafinpatino@Serafins-MacBook-Pro.local>
2023-11-10 16:59:20 -0600

    refactored header and stats button

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect, useCallback } from "react";
 import CurrentUserGame from "./components/CurrentUserGame";
 import ExternalUserGame from "./components/ExternalUserGame";
-import { newGameApi, getExternalUserGamesApi } from "./components/util/apiCalls";
+import {
+  newGameApi,
+  getExternalUserGamesApi,
+} from "./components/util/apiCalls";
 import { useAuth0 } from "@auth0/auth0-react";
 import ErrorBadge from "./components/ErrorBadge";
+import Stats from "./components/Stats";
+import Layout from "./components/Layout";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   const { isAuthenticated, user } = useAuth0();
   const [gameState, setGameState] = useState(null);
-  const [externalGamesState, setExternalGamesState] = useState(null)
-  const [error, setError] = useState(null)
-  // TODO replace this with multiple pages 
+  const [externalGamesState, setExternalGamesState] = useState(null);
+  const [error, setError] = useState(null);
+  // TODO replace this with multiple pages
   const [isMultiplayerEnabled, setIsMultiplayerEnabled] = useState(true);
 
   const initialGameState = useCallback(async () => {
@@ -21,7 +27,7 @@ function App() {
       const initialData = await newGameApi(maxGuesses, wordLength, user.sub);
       setGameState(initialData);
     } catch (error) {
-      setError("Failed to initialize game state: " + error)
+      setError("Failed to initialize game state: " + error);
     }
   }, [isAuthenticated]);
 
@@ -33,34 +39,61 @@ function App() {
   // Fetch state of externalUserGames for multiplayer when the current user submits a guess
   // (the game state in the CurrentUserGame component changes)
   useEffect(() => {
-    if (gameState != null && gameState.metadata.gameID != null && isMultiplayerEnabled) {
+    if (
+      gameState != null &&
+      gameState.metadata.gameID != null &&
+      isMultiplayerEnabled
+    ) {
       const fetchNewExternalUserGames = async () => {
         try {
           const res = await getExternalUserGamesApi(gameState.metadata.gameID);
           setExternalGamesState(res);
+        } catch (e) {
+          setError("Failed to retrieve external user games" + e);
         }
-        catch (e) {
-          setError("Failed to retrieve external user games" + e)
-        }
-      }
+      };
 
       fetchNewExternalUserGames();
     }
-  }, [gameState, isMultiplayerEnabled])
+  }, [gameState, isMultiplayerEnabled]);
 
   // TODO make this component the landing page for two separate pages
   // 1) A single player page where CurrentUserGame is the only component (with Vassle header still)
   // 2) A multiplayer page where the CurrentUserGame and x amount of ExternalUserGame components
   // are on the same page
   return (
-    <div className="App">
-      <h1>Vaasle</h1>
-      {error != null && <ErrorBadge text={error} />}
-      <CurrentUserGame gameState={gameState} setGameState={setGameState} />
-      {isMultiplayerEnabled && isAuthenticated && externalGamesState != null && Array.from(externalGamesState.externalUserGamesMap.values()).map((game) =>
-        <ExternalUserGame externalUserGameGuesses={game} />
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <CurrentUserGame
+                gameState={gameState}
+                setGameState={setGameState}
+              />
+              {isMultiplayerEnabled &&
+                isAuthenticated &&
+                externalGamesState != null &&
+                Array.from(
+                  externalGamesState.externalUserGamesMap.values()
+                ).map((game) => (
+                  <ExternalUserGame externalUserGameGuesses={game} />
+                ))}
+            </Layout>
+          }
+        />
+        <Route
+          path="/stats"
+          element={
+            <Layout>
+              <Stats />
+            </Layout>
+          }
+        />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
