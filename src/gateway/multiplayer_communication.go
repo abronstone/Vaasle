099c1dde1cfe2fe8eef8ffc34e3ec992763580f7
Aidan Roessler <87043349+AidanRoessler@users.noreply.github.com>
2023-11-12 12:54:58 -0600

    155 Improve Gateway Structure and Documentation (#156)
    * Create engine_communication and user_communication
    
    * Add detailed comments

--- src/gateway/multiplayer_communication.go
+++ src/gateway/multiplayer_communication.go
@@ -10,6 +10,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
+Creates a new multiplayer game instance.
+Sends a POST request to the 'newMultiplayerGame' endpoint with custom game metadata.
+On success, returns the initial state of the new multiplayer game.
+Handles errors by returning appropriate HTTP status codes and error messages.
+
+@param: Custom game metadata in the request body
+@return: Initial multiplayer game state or an error message
+*/
 func api_createMultiplayerGame(c *gin.Context) {
 	newGameCustomMetadata := structs.GameMetadata{}
 	if err := c.ShouldBindJSON(&newGameCustomMetadata); err != nil {
@@ -48,6 +57,15 @@ func api_createMultiplayerGame(c *gin.Context) {
 	c.JSON(http.StatusOK, game.GetShareable())
 }
 
+/*
+Joins an existing multiplayer game.
+Retrieves the game ID from the URL, sends a PUT request to the 'joinMultiplayerGame' endpoint with the provided metadata.
+On successful joining, returns the current state of the multiplayer game.
+Handles errors by returning appropriate HTTP status codes and error messages.
+
+@param: Game ID from the URL and custom game metadata in the request body
+@return: Current multiplayer game state or an error message
+*/
 func api_joinMultiplayerGame(c *gin.Context) {
 	id := c.Param("id")
 
@@ -107,6 +125,15 @@ func api_joinMultiplayerGame(c *gin.Context) {
 	c.JSON(http.StatusOK, game.GetShareable())
 }
 
+/*
+Starts an already created multiplayer game.
+Retrieves the game ID from the URL, sends a PUT request to the 'startMultiplayerGame' endpoint.
+Confirms the start of the game by returning a success message.
+Handles errors by returning appropriate HTTP status codes and error messages.
+
+@param: Game ID from the URL
+@return: Confirmation message or an error message
+*/
 func api_startMultiplayerGame(c *gin.Context) {
 	id := c.Param("id")
 	client := &http.Client{}
@@ -138,6 +165,15 @@ func api_startMultiplayerGame(c *gin.Context) {
 	c.JSON(http.StatusOK, structs.Message{Message: "started game"})
 }
 
+/*
+Refreshes the state of an existing multiplayer game.
+Retrieves the game ID from the URL, sends a PUT request to the 'refreshMultiplayerGame' endpoint.
+Returns the latest state of the multiplayer game.
+Handles errors by returning appropriate HTTP status codes and error messages.
+
+@param: Game ID from the URL
+@return: Updated multiplayer game state or an error message
+*/
 func api_refreshMultiplayerGame(c *gin.Context) {
 	id := c.Param("id")
 	client := &http.Client{}
---
+++ src/gateway/user_communication.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+/*
+Creates a new user in the system.
+Receives a request with user details, validates the data, and sends a PUT request to the user creation endpoint.
+Returns a success message upon successful creation, or an error message in case of failure or if the user already exists.
+
+@param: User details in the request body (username and ID)
+@return: Success or error message
+*/
+func api_newUser(c *gin.Context) {
+
+	// Define a struct to bind the request body
+	type NewUserRequestBody struct {
+		UserName string `json:"userName"`
+		Id       string `json:"id"`
+	}
+
+	var requestBody NewUserRequestBody
+
+	// Bind JSON to struct
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	// Create a new HTTP client
+	client := &http.Client{}
+
+	// Convert struct to JSON
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error converting data to JSON"})
+		return
+	}
+
+	// Create a new request
+	req, err := http.NewRequest(http.MethodPut, "http://online:80/create-user", bytes.NewBuffer(jsonData))
+
+	// Handle request creation error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create a new request: " + err.Error()})
+		return
+	}
+
+	// Set Content-Type header
+	req.Header.Set("Content-Type", "application/json")
+
+	// Execute the request
+	res, err := client.Do(req)
+
+	// Handle execution error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to execute user creation request: " + err.Error()})
+		return
+	}
+
+	// Close the response body
+	defer res.Body.Close()
+
+	// Check for status codes
+	if res.StatusCode == http.StatusBadRequest {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
+}
+
+/*
+Handles user login.
+Extracts the username from the URL parameter, sends a PUT request to the login endpoint.
+Returns a success message if login is successful, or an error message if the user does not exist or if there are other login issues.
+
+@param: Username from the URL parameter
+@return: Login success or error message
+*/
+func api_login(c *gin.Context) {
+	var userName string = c.Param("username")
+
+	// Create a new HTTP client
+	client := &http.Client{}
+
+	// Create a new request
+	req, err := http.NewRequest(http.MethodPut, "http://online:80/login/"+userName, nil)
+
+	// Handle request creation error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create a new request: " + err.Error()})
+		return
+	}
+
+	// Execute the request
+	res, err := client.Do(req)
+
+	// Handle execution error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to execute login request: " + err.Error()})
+		return
+	}
+
+	// Close the response body
+	defer res.Body.Close()
+
+	// Check for status codes
+	if res.StatusCode == http.StatusUnauthorized {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User does not exist"})
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error logging in"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
