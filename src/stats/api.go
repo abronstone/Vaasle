536cbad875c6e6845ed282947f7a2e43279cb386
Varun Saini <61795485+VarunSaini02@users.noreply.github.com>
2023-11-15 01:23:37 -0600

    Finish implementing stats container
    - Reimplemented with the help of changes to the Mongo API

--- src/stats/api.go
+++ src/stats/api.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
-	"strconv"
 	"vaas/structs"
 
 	"github.com/gin-gonic/gin"
@@ -22,7 +18,6 @@ func main() {
 
 	router.GET("/", api_home)
 	router.GET("/getStats/:userId", api_getStats)
-	router.GET("/getLeaderboard/:maxNumUsers", api_getLeaderboard)
 
 	router.Run("0.0.0.0:5001")
 }
@@ -34,117 +29,35 @@ func api_home(c *gin.Context) {
 
 // Gets relevant stats for the given user.
 func api_getStats(c *gin.Context) {
-	userId := c.Param("userId")
-
-	// TODO: query the mongo container to get relevant stats for user: userId
-	// TODO: aggregate data into presentable statistics for the frontend to display
-
-	// Asks the Mongo API (mongo.go) for the game stored under the given ID.
-	// func mongo_getGame(id string) (*structs.Game, error) {
-	// 1. Send request
-
-	endpoint := "http://mongo:8000/get-user/" + userId
-
-	res, err := http.Get(endpoint)
+	user, err := mongo_getUser(c.Param("userId"))
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, structs.Message{Message: "Error getting user stats" + err.Error()})
-	}
-	defer res.Body.Close()
-
-	// 2. Parse response body
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, structs.Message{Message: "Error getting user stats" + err.Error()})
-	}
-
-	user := &structs.User{}
-	err = json.Unmarshal(bodyBytes, user)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, structs.Message{Message: "Error getting user stats" + err.Error()})
-	}
-
-	games := user.Games
-	gamesPlayed := user.NumGames
-	gamesWon := 0
-	guesses := map[string]int{}
-
-	for element := range games {
-		var thisgame *structs.Game
-		thisgame, err = mongo_getGame(element)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, structs.Message{Message: "Error getting user stats" + err.Error()})
-		}
-		for _, value := range thisgame.Guesses {
-			count, ok := guesses[value[0]]
-			if ok {
-				guesses[value[0]] = count + 1
-			}
-			if !ok {
-				guesses[value[0]] = 1
-			}
-
-		}
-		if thisgame.State == "won" {
-			gamesWon += 1
-		}
-	}
-
-	var winPercentage = float32(gamesWon) / float32(gamesPlayed)
-
-	var mostCommonWord = ""
-	var num = 0
-	for key, value := range guesses {
-		if value > num {
-			mostCommonWord = key
-			num = value
-		}
-	}
-	returnval := structs.IndividualUserStats{GamesPlayed: gamesPlayed, WinPercentage: winPercentage, MostGuessedWord: mostCommonWord}
-	// returnval.GamesPlayed = gamesPlayed
-	// returnval.WinPercentage = winPercentage
-	// returnval.MostGuessedWord = mostCommonWord
-
-	c.JSON(http.StatusOK, returnval)
-}
-
-// Asks the Mongo API (mongo.go) for the game stored under the given ID.
-func mongo_getGame(id string) (*structs.Game, error) {
-	// 1. Send request
-	endpoint := "http://mongo:8000/get-game/" + id
-
-	res, err := http.Get(endpoint)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	// 2. Parse response body
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	game := &structs.Game{}
-	err = json.Unmarshal(bodyBytes, game)
-	if err != nil {
-		return nil, err
-	}
-
-	return game, nil
-}
-
-// Returns the top maxNumUsers in descending order with respect to total games played.
-func api_getLeaderboard(c *gin.Context) {
-	maxNumUsers, err := strconv.Atoi(c.Param("maxNumUsers"))
-	if err != nil {
-		errorMsg := fmt.Sprintf("error: maxNumUsers must be an int, got %s", c.Param("maxNumUsers"))
-		c.JSON(http.StatusBadRequest, structs.Message{Message: errorMsg})
+		c.JSON(http.StatusInternalServerError, structs.Message{Message: err.Error()})
 		return
 	}
 
-	// TODO: delete this
-	message := fmt.Sprintf("received request for leaderboard of %d users", maxNumUsers)
-	c.JSON(http.StatusOK, structs.Message{Message: message})
+	words, err := mongo_getCommonWords(user.Id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, structs.Message{Message: err.Error()})
+		return
+	}
 
-	// TODO: query the mongo container to get the top maxNumUsers with respect to total games played.
+	mostCommonWord := ""
+	highestFrequency := 0
+	for _, word := range words {
+		if word.Count > highestFrequency {
+			mostCommonWord = word.Word
+		}
+	}
+
+	winPercentage := float32(0)
+	if user.NumGamesFinished != 0 {
+		winPercentage = float32(user.NumGamesWon) / float32(user.NumGamesFinished)
+	}
+
+	c.JSON(http.StatusOK, &structs.IndividualUserStats{
+		GamesPlayed:              user.NumGamesFinished,
+		WinPercentage:            winPercentage,
+		MostGuessedWord:          mostCommonWord,
+		MostGuessedWordFrequency: highestFrequency,
+	})
 }
--- src/stats/mongo_communication.go
+++ src/stats/mongo_communication.go
@@ -30,3 +30,29 @@ func mongo_getUser(userId string) (*structs.User, error) {
 
 	return user, nil
 }
+
+// Asks the Mongo API (mongo.go) for the most common words for the given user.
+func mongo_getCommonWords(userId string) ([]structs.CommonWordFrequency, error) {
+	// 1. Send request
+	endpoint := "http://mongo:8000/most-common-words/" + userId
+
+	res, err := http.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	// 2. Parse response body
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	words := make([]structs.CommonWordFrequency, 0)
+	err = json.Unmarshal(bodyBytes, &words)
+	if err != nil {
+		return nil, err
+	}
+
+	return words, nil
+}
