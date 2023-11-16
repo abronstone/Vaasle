03bff6acd45851049cd88c8abbd0c35153afc2ed
Serafin Patino <serafinpatino@Serafins-MacBook-Pro.local>
2023-11-15 23:30:54 -0600

    base getStatsapi

--- src/frontend/src/components/util/apiCalls.js
+++ src/frontend/src/components/util/apiCalls.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 /**
  * Calls gateway's POST /newGame endpoint
- * 
- * @param {number} maxGuesses 
- * @param {number} wordLength 
- * @param {string} userId 
+ *
+ * @param {number} maxGuesses
+ * @param {number} wordLength
+ * @param {string} userId
  * @returns data object from the POST /newGame response
  */
 export const newGameApi = async (maxGuesses, wordLength, userId) => {
@@ -40,9 +40,9 @@ export const newGameApi = async (maxGuesses, wordLength, userId) => {
 
 /**
  * Calls gateway's POST /makeGuess endpoint
- * 
- * @param {string} gameId 
- * @param {string} guess 
+ *
+ * @param {string} gameId
+ * @param {string} guess
  * @returns data object from the gateway POST /makeGuess endpoint
  */
 export const makeGuessApi = async (gameId, guess) => {
@@ -76,18 +76,22 @@ export const makeGuessApi = async (gameId, guess) => {
 
 /**
  * Calls gateway to create a user with the given userName
- * 
- * @param {string} userName 
- * @param {string} userId 
+ *
+ * @param {string} userName
+ * @param {string} userId
  * @returns {boolean} true if the user was created, false otherwise
  */
 
 export const createUserApi = async (userName, userId) => {
   try {
-    const res = await axios.put("http://localhost:5002/createUser", {
-      userName: userName,
-      id: userId,
-    }, { headers: { "Content-Type": "application/json", } });
+    const res = await axios.put(
+      "http://localhost:5002/createUser",
+      {
+        userName: userName,
+        id: userId,
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
 
     if (res.status !== 200) {
       return false;
@@ -100,8 +104,8 @@ export const createUserApi = async (userName, userId) => {
 
 /**
  * Calls gateway to (login) validate that the user exists in the database
- * 
- * @param {string} userName 
+ *
+ * @param {string} userName
  * @returns {boolean} true if you can login, false otherwise
  */
 export const loginApi = async (userName) => {
@@ -117,36 +121,36 @@ export const loginApi = async (userName) => {
 };
 
 export const getExternalUserGamesApi = async (gameId) => {
-  const externalUserIds = ['123456', '109876', '234567', '987654', ];
+  const externalUserIds = ["123456", "109876", "234567", "987654"];
   // ['345678', '876543', '8525001', '8525002']
   const externalUserGamesMap = new Map();
-  const state = 'ongoing';
+  const state = "ongoing";
 
   // User 123456's guesses
-  externalUserGamesMap.set('123456', [
-    ['G', 'Y', 'X', 'G', 'Y'],
-    ['Y', 'Y', 'X', 'Y', 'Y'],
-    ['Y', 'Y', 'X', 'Y', 'Y']
+  externalUserGamesMap.set("123456", [
+    ["G", "Y", "X", "G", "Y"],
+    ["Y", "Y", "X", "Y", "Y"],
+    ["Y", "Y", "X", "Y", "Y"],
   ]);
 
   // User 109876's guesses
-  externalUserGamesMap.set('109876', [
-    ['X', 'Y', 'G', 'Y', 'G'],
-    ['Y', 'G', 'Y', 'G', 'Y']
+  externalUserGamesMap.set("109876", [
+    ["X", "Y", "G", "Y", "G"],
+    ["Y", "G", "Y", "G", "Y"],
   ]);
 
   // User 234567's guesses
-  externalUserGamesMap.set('234567', [
-    ['G', 'G', 'Y', 'X', 'Y'],
-    ['Y', 'Y', 'Y', 'G', 'X'],
-    ['X', 'X', 'G', 'G', 'Y']
+  externalUserGamesMap.set("234567", [
+    ["G", "G", "Y", "X", "Y"],
+    ["Y", "Y", "Y", "G", "X"],
+    ["X", "X", "G", "G", "Y"],
   ]);
 
   // User 987654's guesses
-  externalUserGamesMap.set('987654', [
-    ['Y', 'X', 'X', 'Y', 'G'],
-    ['G', 'Y', 'Y', 'X', 'Y'],
-    ['Y', 'G', 'Y', 'G', 'G']
+  externalUserGamesMap.set("987654", [
+    ["Y", "X", "X", "Y", "G"],
+    ["G", "Y", "Y", "X", "Y"],
+    ["Y", "G", "Y", "G", "G"],
   ]);
 
   // // Adding new user 345678's guesses
@@ -175,10 +179,22 @@ export const getExternalUserGamesApi = async (gameId) => {
   //   ['G', 'G', 'Y', 'Y', 'X']
   // ])
 
-
   return {
     externalUserIds,
     externalUserGamesMap,
     state,
+  };
+};
+
+export const getStatsApi = async (userId) => {
+  try {
+    const res = await axios.get("http://localhost:5002/getStats/" + userId);
+    if (res.status !== 200) {
+      throw new Error(`HTTP ERROR! Status: ${res.status}`);
+    }
+    return res.data;
+  } catch (error) {
+    console.error("Fetch failed");
+    throw new Error("Fetch failed", error);
   }
 };
